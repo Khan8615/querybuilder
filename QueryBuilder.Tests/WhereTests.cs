e8aa192e747fa9ca290041a048fffe40baad2955
Khan <kfarris8@gmail.com>
2022-07-11 04:38:15 -0700

    Fixed Compile Issue
    Updated to .net 4.8

--- QueryBuilder.Tests/WhereTests.cs
+++ QueryBuilder.Tests/WhereTests.cs
@@ -9,11 +9,11 @@ namespace SqlKata.Tests
         [Fact]
         public void GroupedWhereFilters()
         {
-            var q = new Query("Table1")
+            var x = new Query("Table1")
                 .Where(q => q.Or().Where("Column1", 10).Or().Where("Column2", 20))
                 .Where("Column3", 30);
 
-            var c = Compile(q);
+            var c = Compile(x);
 
             Assert.Equal(@"SELECT * FROM ""Table1"" WHERE (""Column1"" = 10 OR ""Column2"" = 20) AND ""Column3"" = 30", c[EngineCodes.PostgreSql]);
         }
@@ -21,11 +21,11 @@ namespace SqlKata.Tests
         [Fact]
         public void GroupedHavingFilters()
         {
-            var q = new Query("Table1")
+            var x = new Query("Table1")
                 .Having(q => q.Or().HavingRaw("SUM([Column1]) = ?", 10).Or().HavingRaw("SUM([Column2]) = ?", 20))
                 .HavingRaw("SUM([Column3]) = ?", 30);
 
-            var c = Compile(q);
+            var c = Compile(x);
 
             Assert.Equal(@"SELECT * FROM ""Table1"" HAVING (SUM(""Column1"") = 10 OR SUM(""Column2"") = 20) AND SUM(""Column3"") = 30", c[EngineCodes.PostgreSql]);
         }
